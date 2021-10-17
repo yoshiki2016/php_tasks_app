ff63372ac8a194c022dc91078d49eb97cdf74e61
Yoshiki2016 <785431ys@gmail.com>
2021-10-17 16:11:47 +0900

    セクション「#07 PDOの設定をしてみよう」が完了

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -1,3 +1,22 @@
+<?php
+define('DNS', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+
+try {
+  $pdo = new PDO(
+    DSN,
+    DB_USER,
+    DB_PASS,
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]
+  );
+} catch (PDOException $e) {
+  echo $e->getMesage();
+  exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
