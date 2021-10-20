b866644d25c1b6afca7befeb3448e48aa2da2220
Yoshiki2016 <785431ys@gmail.com>
2021-10-20 18:22:20 +0900

    セクション「#15 トークンの挙動を確認しよう 」が完了

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -1,9 +1,13 @@
 <?php
 
+session_start();
+
 define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+
+createToken();
 
 try {
   $pdo = new PDO(
@@ -25,6 +29,24 @@ function h($str)
 {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
+function createToken()
+{
+  if (!isset($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken()
+{
+  if (
+    empty($_SESSION['token']) ||
+    $_SESSION['token'] !== filter_input(INPUT_POST, 'token')
+  ) {
+    exit('Invalid post request');
+  }
+}
+
 function addTodos($pdo)
 {
   $title = trim(filter_input(INPUT_POST, 'title'));
@@ -45,6 +67,7 @@ function getTodos($pdo)
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+  validateToken();
   addTodos($pdo);
 
   header('Location: SITE_URL');
@@ -65,6 +88,7 @@ $todos = getTodos($pdo);
 
     <form action="" method="post">
       <input type="text" name="title" placeholder="Type new todo.">
+      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
     </form>
 
     <ul>
