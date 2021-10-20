b0b8cac4d00a676f956a54b36a5baa79fbfbe163
Yoshiki2016 <785431ys@gmail.com>
2021-10-20 18:27:18 +0900

    セクション「16 設定ファイルを切り出そう」が完了

---
+++ todo_app_php_v4-master/work/app/config.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+
