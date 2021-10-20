92e6bc69465a3729516427a225da05e2c074076a
Yoshiki2016 <785431ys@gmail.com>
2021-10-20 20:24:31 +0900

    #25 リストのスタイルを整えよう

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -38,32 +38,34 @@ $todos = getTodos($pdo);
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
-    <h1>Todos</h1>
+    <main>
+      <h1>Todos</h1>
 
-    <form action="?action=add" method="post">
-      <input type="text" name="title" placeholder="Type new todo.">
-      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
-    </form>
+      <form action="?action=add" method="post">
+        <input type="text" name="title" placeholder="Type new todo.">
+        <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+      </form>
 
-    <ul>
-      <?php foreach ($todos as $todo): ?>
-        <li>
-          <form action="?action=toggle" method="POST">
-            <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-            <input type="hidden" name="id" value="<?= h($todo->id); ?>">
-            <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
-          </form>
-          <span class="<?= $todo->is_done ? 'done' : ''; ?>">
-            <?= h($todo->title); ?>
-          </span>
-          <form action="?action=delete" method="POST">
-            <span class="delete">x</span>
-            <input type="hidden" name="id" value="<?= h($todo->id); ?>">
-            <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
-          </form>
-        </li>
-      <?php endforeach; ?>
-    </ul>
+      <ul>
+        <?php foreach ($todos as $todo): ?>
+          <li>
+            <form action="?action=toggle" method="POST">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <input type="hidden" name="id" value="<?= h($todo->id); ?>">
+              <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+            </form>
+            <span class="<?= $todo->is_done ? 'done' : ''; ?>">
+              <?= h($todo->title); ?>
+            </span>
+            <form action="?action=delete" method="POST" class="delete-form">
+              <span class="delete">x</span>
+              <input type="hidden" name="id" value="<?= h($todo->id); ?>">
+              <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+            </form>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </main>
     <script src="./js/main.js"></script>
   </body>
 </html>
