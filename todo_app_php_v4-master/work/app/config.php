81f414f0177c8a87e57b5ee2561c0043707ef20e
Yoshiki2016 <785431ys@gmail.com>
2021-10-23 17:16:51 +0900

    セクション「#09 クラスを自動的に読み込もう」が完了

--- todo_app_php_v4-master/work/app/config.php
+++ todo_app_php_v4-master/work/app/config.php
@@ -5,7 +5,15 @@ define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-require_once(__DIR__ . '/Utils.php');
-require_once(__DIR__ . '/Token.php');
-require_once(__DIR__ . '/Database.php');
-require_once(__DIR__ . '/Todo.php');
+spl_autoload_register(
+  function ($class) {
+    $fileName = sprintf(__DIR__ . '/%s.php', $class);
+
+    if (file_exists($fileName)) {
+      require($fileName);
+    } else {
+      echo 'File not found: ' . $fileName;
+      exit;
+    }
+  }
+);
