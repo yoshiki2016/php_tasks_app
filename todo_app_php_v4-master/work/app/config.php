ddcf021f47ddfdd20ae6a0561ade3b2c69b96b9c
Yoshiki2016 <785431ys@gmail.com>
2021-10-23 16:52:55 +0900

    セクション「#05 Databaseクラスを実装しよう」が完了

--- todo_app_php_v4-master/work/app/config.php
+++ todo_app_php_v4-master/work/app/config.php
@@ -5,6 +5,7 @@ define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/Utils.php');
 require_once(__DIR__ . '/Token.php');
+require_once(__DIR__ . '/Database.php');
+require_once(__DIR__ . '/functions.php');
--- todo_app_php_v4-master/work/app/functions.php
+++ todo_app_php_v4-master/work/app/functions.php
@@ -42,23 +42,3 @@ function getTodos($pdo)
   $todos = $stmt->fetchAll();
   return $todos;
 }
-
-function getPdoInstance()
-{
-  try {
-    $pdo = new PDO(
-      DSN,
-      DB_USER,
-      DB_PASS,
-      [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        PDO::ATTR_EMULATE_PREPARES => false,
-      ]
-    );
-    return $pdo;
-  } catch (PDOException $e) {
-    echo $e->getMesage();
-    exit;
-  }
-}
